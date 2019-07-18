6ded002ee16a7796f1120032499304f5f6475285
Ryan McCarthy <ryanxmccarthy@gmail.com>
2019-07-17 22:10:38 -0700

    3 solved

---
+++ javascript/7.js
@@ -0,0 +1,4 @@
+var reverse = function(x) {
+    const reversed =  parseInt(Math.abs(x).toString().split('').reverse().join('')) * Math.sign(x);
+    return (reversed <= 0x7fffffff && reversed >= -0x80000000) ? reversed : 0;
+};
