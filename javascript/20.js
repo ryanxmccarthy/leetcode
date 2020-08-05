f82702e406d01b96b0f05db1545f28e88b52d7d9
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-04 19:52:01 -0700

    20 solved

--- javascript/20.js
+++ javascript/20.js
@@ -6,4 +6,22 @@ var isValid = function (s) {
   if (s === "") {
     return true;
   }
+
+  let bracket = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+
+  let heap = [];
+
+  for (let char of s) {
+    if (bracket[char]) {
+      heap.push(bracket[char]);
+    } else {
+      if (heap.pop() !== char) return false;
+    }
+  }
+
+  return !heap.length;
 };
