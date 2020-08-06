8206cc7dbf5b221cf49e929f1508446ca80d9e28
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-05 18:33:14 -0700

    solved 21

--- javascript/20-27/21.js
+++ javascript/20-27/21.js
@@ -10,4 +10,33 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function (l1, l2) {};
+const mergeTwoLists = function (l1, l2) {
+  let dummyHead = new ListNode();
+  let current = dummyHead;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+
+    current = current.next;
+  }
+
+  while (l1) {
+    current.next = l1;
+    l1 = l1.next;
+    current = current.next;
+  }
+
+  while (l2) {
+    current.next = l2;
+    l2 = l2.next;
+    current = current.next;
+  }
+
+  return dummyHead.next;
+};
