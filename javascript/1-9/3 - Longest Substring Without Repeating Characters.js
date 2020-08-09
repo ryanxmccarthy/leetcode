30ca6854f3aff524a4bd4f8e444ab0d08642a87b
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-09 16:43:39 -0700

    sliding window for 3

--- javascript/1-9/3 - Longest Substring Without Repeating Characters.js
+++ javascript/1-9/3 - Longest Substring Without Repeating Characters.js
@@ -4,7 +4,7 @@
  */
 
 //brute force solution
-var lengthOfLongestSubstring = function (s) {
+var lengthOfLongestSubstringBrute = function (s) {
   let count = 0;
 
   for (let i = 0; i < s.length; i++) {
@@ -26,5 +26,28 @@ var lengthOfLongestSubstring = function (s) {
   return count;
 };
 
-let result = lengthOfLongestSubstring("abcabcbb");
-console.log(result);
+//sliding window
+var lengthOfLongestSubstringWindow = function (s) {
+  let count = 0;
+  let i = 0;
+  let j = 0;
+  let n = s.length;
+  let set = new Set();
+
+  while (i < n && j < n) {
+    let char = s.charAt(j);
+    if (!set.has(char)) {
+      set.add(char);
+      j++;
+      count = Math.max(count, j - i);
+    } else {
+      set.delete(s.charAt(i));
+      i++;
+    }
+  }
+
+  return count;
+};
+
+console.log(lengthOfLongestSubstringBrute("abcabcbb"));
+console.log(lengthOfLongestSubstringWindow("abcabcbb"));
