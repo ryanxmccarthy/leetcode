2b4c87d135619f90de532902d201525bcb7a83c0
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-11 14:23:27 -0700

    1480 solved

---
+++ javascript/1400-1499/1480-1489/1480 - Running Sum of 1d Array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  let runningSums = [];
+  let runningSum = 0;
+  for (var i = 0; i < nums.length; i++) {
+    runningSum += nums[i];
+    runningSums.push(runningSum);
+  }
+  return runningSums;
+};
