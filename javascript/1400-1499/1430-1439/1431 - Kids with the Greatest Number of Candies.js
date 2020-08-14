02cb6589ed227ad3295d2a1fd27a13f4e5af937d
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-13 18:25:57 -0700

    1431 solved

--- javascript/1400-1499/1430-1439/1431 - Kids with the Greatest Number of Candies.js
+++ javascript/1400-1499/1430-1439/1431 - Kids with the Greatest Number of Candies.js
@@ -3,4 +3,23 @@
  * @param {number} extraCandies
  * @return {boolean[]}
  */
-var kidsWithCandies = function (candies, extraCandies) {};
+var kidsWithCandies = function (candies, extraCandies) {
+  let results = [];
+  let max = 0;
+
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] > max) {
+      max = candies[i];
+    }
+  }
+
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max) {
+      results.push(true);
+    } else {
+      results.push(false);
+    }
+  }
+
+  return results;
+};
