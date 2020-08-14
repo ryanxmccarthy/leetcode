5cbe1ef4194c31960442bcc72b77d1e49c215f3e
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-14 14:58:57 -0700

    1470 solved

--- javascript/1400-1499/1470-1479/1470. Shuffle the Array.js
+++ javascript/1400-1499/1470-1479/1470. Shuffle the Array.js
@@ -3,4 +3,12 @@
  * @param {number} n
  * @return {number[]}
  */
-var shuffle = function (nums, n) {};
+var shuffle = function (nums, n) {
+  let output = [];
+
+  for (let i = 0; i < nums.length / 2; i++) {
+    output.push(nums[i], nums[i + n]);
+  }
+
+  return output;
+};
