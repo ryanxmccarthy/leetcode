055dad50f4ce49d5ee7f68537a31f843773b2d6a
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-15 22:00:46 -0700

    1512 solved

---
+++ javascript/1512 - Number of Good Pairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function (nums) {
+  let pairs = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i < j && nums[i] == nums[j]) {
+        pairs++;
+      }
+    }
+  }
+  return pairs;
+};
