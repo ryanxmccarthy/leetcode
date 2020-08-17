cfb0edffb1e447e812289e113e26cf06672d0dc1
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-17 16:47:13 -0700

    771 solved

--- javascript/700-799/771 - Jewels and Stones.js
+++ javascript/700-799/771 - Jewels and Stones.js
@@ -3,4 +3,16 @@
  * @param {string} S
  * @return {number}
  */
-var numJewelsInStones = function (J, S) {};
+var numJewelsInStones = function (J, S) {
+  let stones = S.split("");
+  let jewels = J.split("");
+  let count = 0;
+
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.includes(stones[i])) {
+      count++;
+    }
+  }
+
+  return count;
+};
