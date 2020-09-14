8dbe7dd58938e3815fc275b8c73e6fa34df3fdd2
Ryan XVX <ryanxmccarthy@gmail.com>
2020-09-13 19:09:46 -0700

    1266 solved

---
+++ javascript/1200-1299/1266. Minimum Time Visiting All Points.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+let minTimeToVisitAllPoints = function(points) {
+    let i = 0, result = 0;
+    while(i < points.length - 1) {
+        let x= Math.abs(points[i][0] - points[i+1][0]);
+        let y= Math.abs(points[i][1] - points[i+1][1]);
+        result = result + Math.max(x,y);
+        i++;
+    }
+
+    return result;
+};
