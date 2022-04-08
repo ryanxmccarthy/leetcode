f97e6f09baf6a02c32732b89f062fcc6f5113a67
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-07 22:04:38 -0700

    solved with some help

--- javascript/1-99/1-9/5 - Longest Palindromic Substring.js
+++ javascript/1-99/1-9/5 - Longest Palindromic Substring.js
@@ -4,25 +4,46 @@
  * @param {string} s
  * @return {string}
  */
- var longestPalindrome = function(s) {
-    if (s.length < 1) return '';
 
-    let longestPalindrome = '';
+ var longestPalindrome = function (string) {
+    let longestPalLength = 0;
+    let longestPalLeft = 0;
+    let longestPalRight = 0;
 
     var getLongestPalindrome = function (leftPosition, rightPosition) {
-        while ( 
+        // While there is space to expand, and the expanded strings match
+        while (
             leftPosition >= 0 &&
-            rightPosition < s.length &&
-            s[leftPosition] === s[rightPosition]
+            rightPosition < string.length &&
+            string[leftPosition] === string[rightPosition]
         ) {
+            //expand in each direction.
             leftPosition--;
             rightPosition++;
         }
 
-        if (rightPosition - leftPosition > longestPalindrome) {
+        // Store the longest palindrome (if it's the longest one found so far)
+        if (rightPosition - leftPosition > longestPalLength) {
             longestPalLeft = leftPosition + 1;
             longestPalRight = rightPosition - 1;
             longestPalLength = longestPalRight - longestPalLeft + 1;
         }
+    };
+
+    // Loop through the letters
+    for (let i = 0; i < string.length; i++) {
+        // Find the longest odd palendrome
+        getLongestPalindrome(i, i + 1);
+
+        // Find the longest even palendrome
+        getLongestPalindrome(i, i);
+
+        // Check if a longer palindrome cannot be found
+        if ((string.length - i) * 2 < longestPalLength) {
+            // Break out to avoid unnecessary computation
+            break;
+        }
     }
+
+    return string.slice(longestPalLeft, longestPalRight + 1);
 };
