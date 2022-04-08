2f60fd80b133e7e3a22365fafb3c6f69fd9f09b0
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-07 21:33:22 -0700

    longest palindromic substring

---
+++ javascript/1-99/1-9/5 - Longest Palindromic Substring.js
@@ -0,0 +1,9 @@
+//Given a string s, return the longest palindromic substring in s.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestPalindrome = function(s) {
+    if (s.length < 1) return '';
+};
