4006c5b5ecb51381d4a3d12b2e123cf68632347a
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-20 11:42:44 -0700

    optimized solution

--- javascript/1-99/50-59/58 - Length of Last Word.js
+++ javascript/1-99/50-59/58 - Length of Last Word.js
@@ -1,10 +1,15 @@
-/**
- * @param {string} s
- * @return {number}
- */
- var lengthOfLastWord = function(s) {
-    const words = s.trim().split(' ');
-    const lastWord = words.pop();
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+//  var lengthOfLastWord = function(s) {
+//     const words = s.trim().split(' ');
+//     const lastWord = words.pop() || '';
 
-    return lastWord.length;
+//     return lastWord.length;
+// };
+
+//optimized solution
+var lengthOfLastWord = function(s) {
+    return s ? s.trim().split(' ').pop().length : 0;
 };
