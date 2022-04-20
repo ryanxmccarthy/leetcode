16ad009464103a59a3b4012c22c7d92a375321f1
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-19 18:58:30 -0700

    using local max to solve

--- javascript/1-99/50-59/53 Maximum Subarray.js
+++ javascript/1-99/50-59/53 Maximum Subarray.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
  var maxSubArray = function(nums) {
-    
+    let localMax = 0;
+    let max = Number.NEGATIVE_INFINITY;
     
     for (let i = 0; i < nums.length; i++) {
-
+        localMax = Math.max(nums[i], nums[i] + localMax);
     }
 };
