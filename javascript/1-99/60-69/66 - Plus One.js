3c0dc32233782f31542f2c8c2ccf554717d8012b
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-21 15:57:25 -0700

    problem solved

--- javascript/1-99/60-69/66 - Plus One.js
+++ javascript/1-99/60-69/66 - Plus One.js
@@ -7,6 +7,11 @@
         if (digits[i] < 9) {
             digits[i] += 1;
             return digits;
+        } else {
+            digits[i] = 0;
         }
     }
+
+    digits.unshift(1);
+    return digits;
 };
