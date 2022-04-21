c286c29037823726d2cc3a1f76f79f1ac09bb56b
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-21 15:54:54 -0700

    handling carry operation

--- javascript/1-99/60-69/66 - Plus One.js
+++ javascript/1-99/60-69/66 - Plus One.js
@@ -3,7 +3,10 @@
  * @return {number[]}
  */
  var plusOne = function(digits) {
-    for (let i = 0; i < digits.length; i++) {
-        console.log(digits[i]);
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        }
     }
 };
