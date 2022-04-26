7dcf73dea5af373222eaa05f6bceb67919ebe29d
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-26 10:35:01 -0700

    adding some clarifying comments

--- javascript/1-99/70-79/70 - Climbing Stairs.js
+++ javascript/1-99/70-79/70 - Climbing Stairs.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
  var climbStairs = function(n) {
+    //initializing fibonacci sequence
     const memo = [1,1,2];
 
     for (let i = 3; i <= n; i++) {
+        //fibonacci sequence
         memo[i] = memo[i-1] + memo[i-2];
     }
 
