950b3b43d4fbf6a6e48f25cbf0d643672cdcccf1
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-27 10:42:14 -0700

    had to change value to val

--- javascript/1-99/80-89/83 - Remove Duplicates from Sorted List.js
+++ javascript/1-99/80-89/83 - Remove Duplicates from Sorted List.js
@@ -13,8 +13,12 @@
     let current = head;
 
     while (current && current.next) {
-        if (current.value == current.next.value) {
+        if (current.val == current.next.val) {
+            current.next = current.next.next;
+        } else {
             current = current.next;
         }
     }
+
+    return head;
 };
