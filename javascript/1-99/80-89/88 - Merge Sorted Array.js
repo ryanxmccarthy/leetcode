b2bb31429236cd6c9abfd00ab724f14abd8fd418
Ryan XVX <ryanxmccarthy@gmail.com>
2022-04-28 12:16:53 -0700

    inserting elements correctly; implementing sort now

--- javascript/1-99/80-89/88 - Merge Sorted Array.js
+++ javascript/1-99/80-89/88 - Merge Sorted Array.js
@@ -6,5 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
  var merge = function(nums1, m, nums2, n) {
+    for (let i = 0; i < n; i++) {
+        nums1.splice(i + n, 1, nums2[i]);
+    }
     
+    return nums1;
 };
