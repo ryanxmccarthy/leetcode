6c7146d46947162384005e92802b821bc5685f42
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-04 10:29:23 -0700

    recursively checking depth

---
+++ javascript/100-199/110-119/110 - Balanced Binary Tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isBalanced = function(root) {
+    let result = true;
+
+    let maxDepth = (node) => {
+        if (!node) return 0;
+        let l = maxDepth(node.left), r = maxDepth(node.right);
+        if (Math.abs(l - r) > 1) res = false;
+
+        return 1 + Math.max(l, r)
+    }
+};
