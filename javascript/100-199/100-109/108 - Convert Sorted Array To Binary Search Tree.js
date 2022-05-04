605dad0397070444dfea8716103aa0fd3f3e02bd
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-03 17:43:58 -0700

    solved

--- javascript/100-199/100-109/108 - Convert Sorted Array To Binary Search Tree.js
+++ javascript/100-199/100-109/108 - Convert Sorted Array To Binary Search Tree.js
@@ -13,9 +13,11 @@
  var sortedArrayToBST = function(nums, left = 0, right = nums.length - 1) {
     if (left > right) return null;
     
-    let mid = Math.floor(left + right / 2);
+    let mid = Math.floor((left + right) / 2);
     let root = new TreeNode(nums[mid]);
 
     root.left = sortedArrayToBST(nums, left, mid - 1);
-    root.right = sortedArrayToBST(nums, right, mid - 1);
+    root.right = sortedArrayToBST(nums, mid + 1, right);
+
+    return root;
 };
