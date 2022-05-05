7cadbc28e66822003a9f430c49dffee9cfe82ae8
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-05 12:52:52 -0700

    solved

--- javascript/100-199/110-119/111 - Minimum Depth of Binary Tree.js
+++ javascript/100-199/110-119/111 - Minimum Depth of Binary Tree.js
@@ -15,6 +15,12 @@
         return 0;
     }
 
-    let leftDepth = minDepth(root.left);
-    let rightDepth = minDepth(root.right);
+    let leftDepth = minDepth(root.left)
+    let rightDepth = minDepth(root.right)
+    
+    if (leftDepth == 0 || rightDepth == 0) {
+        return Math.max(leftDepth, rightDepth) + 1
+    } else {
+        return Math.min(leftDepth, rightDepth) + 1
+    }
 };
