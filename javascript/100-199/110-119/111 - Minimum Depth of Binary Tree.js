26348d0bfb3116fafcbd955e973dbf19a7a5ec43
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-05 12:50:25 -0700

    setting left and right depth

---
+++ javascript/100-199/110-119/111 - Minimum Depth of Binary Tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var minDepth = function(root) {
+    if (root == null) {
+        return 0;
+    }
+
+    let leftDepth = minDepth(root.left);
+    let rightDepth = minDepth(root.right);
+};
