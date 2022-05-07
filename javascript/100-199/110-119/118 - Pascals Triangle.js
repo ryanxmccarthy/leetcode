3290a68189369aacc03a99d63c9a57c1b269a871
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-07 10:00:57 -0700

    trying some solutions

--- javascript/100-199/110-119/118 - Pascals Triangle.js
+++ javascript/100-199/110-119/118 - Pascals Triangle.js
@@ -3,5 +3,14 @@
  * @return {number[][]}
  */
  var generate = function(numRows) {
-    
+    let result = [];
+
+    for (var i = 0; i < numRows; i++) {
+        let curRow = [];
+        if (i = 0) {
+            result.curRow.push(1);
+        }
+    }
+
+    return result;
 };
