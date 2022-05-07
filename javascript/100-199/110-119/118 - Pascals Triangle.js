c128f68b53605c22c58b050a94037651a63a1d90
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-07 10:07:53 -0700

    solved

--- javascript/100-199/110-119/118 - Pascals Triangle.js
+++ javascript/100-199/110-119/118 - Pascals Triangle.js
@@ -10,8 +10,13 @@
         row[0] = 1;
         row[row.length - 1] = 1;
 
-        for (let j = 1; j < row.length; j++) {
-            
+        for (let j = 1; j < row.length - 1; j++) {
+            let rowAbove = pascalsTriangle[i - 1];
+            row[j] = rowAbove[j] + rowAbove[j - 1];
         }
+
+        pascalsTriangle[i] = row;
     }
+
+    return pascalsTriangle;
 };
