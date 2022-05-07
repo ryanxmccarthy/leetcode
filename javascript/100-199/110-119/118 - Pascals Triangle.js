da9ad31b1de4c3df5be12913bfdf7e254514ff85
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-07 10:06:32 -0700

    nested for loop solution

--- javascript/100-199/110-119/118 - Pascals Triangle.js
+++ javascript/100-199/110-119/118 - Pascals Triangle.js
@@ -3,14 +3,15 @@
  * @return {number[][]}
  */
  var generate = function(numRows) {
-    let result = [];
+    let pascalsTriangle = new Array(numRows);
 
-    for (var i = 0; i < numRows; i++) {
-        let curRow = [];
-        if (i = 0) {
-            result.curRow.push(1);
+    for (let i = 0; i < numRows; i++) {
+        let row = new Array(i + 1);
+        row[0] = 1;
+        row[row.length - 1] = 1;
+
+        for (let j = 1; j < row.length; j++) {
+            
         }
     }
-
-    return result;
 };
