d51edd62b011a5070df45c6d33f39167154de49b
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-10 10:43:07 -0700

    solved with one for loop

--- javascript/100-199/120-129/121 - Best Time to Buy and Sell Stock.js
+++ javascript/100-199/120-129/121 - Best Time to Buy and Sell Stock.js
@@ -4,13 +4,25 @@
  */
 
 //solved but not optimized
- var maxProfit = function(prices) {
+//  var maxProfit = function(prices) {
+//     let maxProf = 0;
+
+//     for (let i = 0; i < prices.length; i++) {
+//         for (let j = i + 1; j < prices.length; j++) {
+//             maxProf = Math.max(maxProf, prices[j] - prices[i]);
+//         }
+//     }
+
+//     return maxProf;
+// };
+
+var maxProfit = function(prices) {
     let maxProf = 0;
+    let min = Infinity;
 
     for (let i = 0; i < prices.length; i++) {
-        for (let j = i + 1; j < prices.length; j++) {
-            maxProf = Math.max(maxProf, prices[j] - prices[i]);
-        }
+        min = Math.min(min, prices[i]);
+        maxProf = Math.max(maxProf, prices[i] - min)
     }
 
     return maxProf;
