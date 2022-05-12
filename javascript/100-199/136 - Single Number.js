c86f2f09b3cb6c9ee529ca738aba19fb1c441478
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-12 13:54:59 -0700

    solved 136

--- javascript/100-199/136 - Single Number.js
+++ javascript/100-199/136 - Single Number.js
@@ -5,4 +5,14 @@
  var singleNumber = function(nums) {
     //solution using hashtable
     const ht = {};
+
+    for (const num of nums) {
+        ht[num] = ht[num] + 1 || 1; 
+    }
+
+    for (const key in ht) {
+        if (ht[key] === 1) {
+            return key;
+        }
+    }
 };
