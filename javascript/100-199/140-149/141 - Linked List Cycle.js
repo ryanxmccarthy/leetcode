3386ced588c9c3b7a62a9fae29c21c8e619b8199
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-13 14:39:14 -0700

    solved using two pointers

--- javascript/100-199/140-149/141 - Linked List Cycle.js
+++ javascript/100-199/140-149/141 - Linked List Cycle.js
@@ -11,5 +11,15 @@
  * @return {boolean}
  */
  var hasCycle = function(head) {
-    
+    let fast = head;
+    let slow = head;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (fast === slow) return true;
+    }
+
+    return false;
 };
