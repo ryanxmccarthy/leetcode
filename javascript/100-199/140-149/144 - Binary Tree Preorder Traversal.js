98be02d8372bd814da9a1f54b2aed03267497298
Ryan XVX <ryanxmccarthy@gmail.com>
2022-05-14 10:27:46 -0700

    adding recursive solution

--- javascript/100-199/140-149/144 - Binary Tree Preorder Traversal.js
+++ javascript/100-199/140-149/144 - Binary Tree Preorder Traversal.js
@@ -26,3 +26,21 @@
 
     return result;
 };
+
+//recursive solution
+// var preorderTraversal = function(root) {
+//     if (!root) return null;
+
+//     const result = [];
+
+//     const preorder = (node) => {
+//         if (!node) return null;
+
+//         result.push(node.val);
+//         if (node.left) preorder(node.left);
+//         if (node.right) preorder(node.right);
+//     }
+
+//     preorder(root);
+//     return result;
+// };
